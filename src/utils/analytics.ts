4191f13eb876e976c34264213abfcaf85c4d5fc7
sathyadharsan <sathyadharsan93@gmail.com>
2026-05-15 15:30:10 +0530

    fix(analytics): Correct Google Apps Script URL typo

--- src/utils/analytics.ts
+++ src/utils/analytics.ts
@@ -1,4 +1,4 @@
-export const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbwtQ83HLKu9OnJeFKdvtDL0AMtBo2xx8SLvJt5hwl2e1oAlDfiXGGAo1KsTGm8uJSXV/exec';
+export const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbwtQ83HLKu9OnJeFKdvtDL0AMtBo2xx8SLvJt5hwl2e1oAIDfiXGGAo1KsTGm8uJSXV/exec';
 
 export const trackEvent = async (eventType: string, details: any = {}) => {
   if (!SCRIPT_URL) {
